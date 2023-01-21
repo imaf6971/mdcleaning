7878b55e69826a1da31a25033d9ce34725383816
Vladislav Mironov <imaf6971@gmail.com>
2023-01-21 13:19:22 +0300

    basic room cleaning graphic

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -12,7 +12,20 @@ export default function Room() {
         : <SectionHeading heading="Комната" />}
       <div className="flex flex-col gap-2 m-4 justify-center">
         <h2 className="text-lg font-medium">График уборки</h2>
-
+        <div className="flex flex-col border rounded-md divide-y">
+          <div className="flex justify-between items-center p-3 hover:shadow transition-shadow">
+            <div className="basis-1/6">1.</div>
+            <div className="basis-2/6">12:30 - 13:30</div>
+            <div className="basis-3/6">Фамилия Имя</div>
+          </div>
+          <div className="flex justify-between items-center p-3 hover:shadow transition-shadow">
+            <div className="basis-1/6">1.</div>
+            <div className="basis-2/6">12:30 - 13:30</div>
+            <div className="basis-3/6">Фамилия Имя</div>
+          </div>
+        </div>
+        <h2 className="text-lg font-medium">QR-код</h2>
+        
       </div>
     </main>
 
