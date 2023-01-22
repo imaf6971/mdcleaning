17d444120c68f285578e7d0c93e3a2a6f15d17cf
Vladislav Mironov <imaf6971@gmail.com>
2023-01-22 18:50:53 +0300

    Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+import { MouseEventHandler, ReactNode } from "react"
+
+type ButtonProps = {
+  onClick?: MouseEventHandler<HTMLButtonElement>
+  children: ReactNode
+}
+
+export default function Button({ onClick, children }: ButtonProps) {
+  return (
+    <button onClick={onClick} className="border rounded-md p-2 hover:shadow transition-shadow focus:bg-gray-200 focus:outline-none focus:ring">
+      {children}
+    </button>
+  );
+}
