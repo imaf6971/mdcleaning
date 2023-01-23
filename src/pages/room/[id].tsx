895a19b61bcd6407e11dc8e61ae2e41a0e05c1d7
Vladislav Mironov <imaf6971@gmail.com>
2023-01-23 15:11:28 +0300

    fetching data from server in room/[id]

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -1,14 +1,38 @@
-import AddCleaningForm from "@/components/AddCleaningForm";
+import AddCleaningModal from "@/components/AddCleaningForm";
 import Button from "@/components/Button";
-import Modal from "@/components/Modal";
 import SectionHeading from "@/components/SectionHeading";
+import { createContext } from "@/server/context";
+import { appRouter } from "@/server/routers/_app";
+import { trpc } from "@/utils/trpc";
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { useQRCode } from "next-qrcode";
-import { useRouter } from "next/router";
 import { useState } from "react";
 
-export default function Room() {
-  const router = useRouter()
-  const { id } = router.query
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{ id: string }>,
+) {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: createContext(),
+    // transformer: superjson, // optional - adds superjson serialization
+  });
+  const id = parseInt(context.params?.id as string);
+  await ssg.rooms.byId.prefetch(id)
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
+}
+
+export default function Room(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  const { id } = props
+  const room = trpc.rooms.byId.useQuery(id);
 
   const { Image } = useQRCode()
 
@@ -16,25 +40,22 @@ export default function Room() {
 
   return (
     <main className="container mx-auto">
-      {id !== undefined
-        ? <SectionHeading heading={`Комната ${id}`} />
-        : <SectionHeading heading="Комната" />}
+      {room.isLoading
+        ? <SectionHeading heading="Комната..." />
+        : <SectionHeading heading={room.data!.title} />}
       <div className="flex flex-col gap-2 m-4 justify-center">
         <div className="flex justify-between">
           <h2 className="text-lg font-medium">График уборки</h2>
           <Button onClick={() => setShowAddCleaningModal(true)}>Добавить</Button>
         </div>
         <div className="flex flex-col border rounded-md divide-y">
-          <div className="flex justify-between items-center p-3 hover:shadow transition-shadow">
-            <div className="basis-1/6">1.</div>
-            <div className="basis-2/6">12:30 - 13:30</div>
-            <div className="basis-3/6">Фамилия Имя</div>
-          </div>
-          <div className="flex justify-between items-center p-3 hover:shadow transition-shadow">
-            <div className="basis-1/6">1.</div>
-            <div className="basis-2/6">12:30 - 13:30</div>
-            <div className="basis-3/6">Фамилия Имя</div>
-          </div>
+          {room.data!.cleanings.map((cleaning, idx) =>
+            <div key={cleaning.id} className="flex justify-between items-center p-3 hover:shadow transition-shadow">
+              <div className="basis-1/6">{idx + 1}.</div>
+              <div className="basis-2/6">{cleaning.from} - {cleaning.to}</div>
+              <div className="basis-3/6">Фамилия Имя</div>
+            </div>
+          )}
         </div>
         <h2 className="text-lg font-medium">QR-код</h2>
         <Image
@@ -49,12 +70,11 @@ export default function Room() {
           }}
         />
       </div>
-      <Modal
-        isVisible={showAddCleaningModal}
-        onClose={() => setShowAddCleaningModal(false)}
-      >
-        <AddCleaningForm />
-      </Modal>
+      <AddCleaningModal roomId={id} isVisible={showAddCleaningModal}
+        onClose={() => {
+          setShowAddCleaningModal(false)
+          room.refetch()
+        }} />
     </main>
 
   )
