da494de3e1f0e484f0b560b0fa383960ace7cf11
Vladislav Mironov <imaf6971@gmail.com>
2023-01-23 15:10:54 +0300

    rooms now fetched from backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,20 @@
 import ListItem from "@/components/ListItem";
 import SectionHeading from "@/components/SectionHeading";
+import { trpc } from "@/utils/trpc";
 import Link from "next/link";
 
 export default function Home() {
+  const rooms = trpc.rooms.list.useQuery()
+
   return (
     <main className="container mx-auto">
       <SectionHeading heading="Комнаты" />
       <div className="flex flex-col gap-2 m-4 justify-center items-center">
-        <Link href="/room/1" className="w-full">
-          <ListItem>Комната 1</ListItem>
-        </Link>
-        <Link href="/" className="w-full">
-          <ListItem>Комната 1</ListItem>
-        </Link>
-        <Link href="/" className="w-full">
-          <ListItem>Комната 1</ListItem>
-        </Link>
+        {rooms.data?.map(room =>
+          <Link key={room.id} href={`/room/${room.id}`} className="w-full">
+            <ListItem>{room.title}</ListItem>
+          </Link>
+        )}
       </div>
     </main>
   )
