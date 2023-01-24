d3bbee14a8c92ef024bd8abd413228f69cd71c6e
Vladislav Mironov <imaf6971@gmail.com>
2023-01-24 11:00:00 +0300

    getting list of rooms on ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import ListItem from "@/components/ListItem";
 import SectionHeading from "@/components/SectionHeading";
+import { createContext } from "@/server/context";
+import { appRouter } from "@/server/routers/_app";
 import { trpc } from "@/utils/trpc";
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import Link from "next/link";
+
+export async function getServerSideProps() {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: createContext(),
+    // transformer: superjson, // optional - adds superjson serialization
+  });
+  await ssg.rooms.list.prefetch()
+  return {
+    props: {
+      trpcState: ssg.dehydrate()
+    }
+  }
+}
 
 export default function Home() {
   const rooms = trpc.rooms.list.useQuery()
