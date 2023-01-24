6dd65b357916008bd50f7fb9e294adf9463b79d9
Vladislav Mironov <imaf6971@gmail.com>
2023-01-24 10:17:17 +0300

    extracted CleaningTable in separate component

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -4,6 +4,7 @@ import SectionHeading from "@/components/SectionHeading";
 import { createContext } from "@/server/context";
 import { appRouter } from "@/server/routers/_app";
 import { trpc } from "@/utils/trpc";
+import { Cleaning } from "@prisma/client";
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { useQRCode } from "next-qrcode";
@@ -33,17 +34,8 @@ export default function Room(
 ) {
   const { id } = props
   const room = trpc.rooms.byId.useQuery(id);
-  const deleteCleaning = trpc.cleanings.deleteById.useMutation();
 
   const { Image } = useQRCode()
-
-  const [showAddCleaningModal, setShowAddCleaningModal] = useState(false)
-  const [isEditing, setIsEditing] = useState(false)
-
-  function handleCleaningDelete(id: number) {
-    deleteCleaning.mutateAsync(id)
-      .then(() => room.refetch());
-  }
 
   return (
     <main className="container mx-auto">
@@ -51,25 +43,7 @@ export default function Room(
         ? <SectionHeading heading="Комната..." />
         : <SectionHeading heading={room.data!.title} />}
       <div className="flex flex-col gap-2 m-4 justify-center">
-        <div className="flex justify-between">
-          <h2 className="text-lg font-medium">График уборки</h2>
-          <div className="flex gap-2">
-            {isEditing
-              ? <Button onClick={() => setIsEditing(false)}>Отмена</Button>
-              : <Button onClick={() => setIsEditing(true)}>Изменить</Button>}
-            <Button onClick={() => setShowAddCleaningModal(true)}>Добавить</Button>
-          </div>
-        </div>
-        <div className="flex flex-col border rounded-md divide-y">
-          {room.data!.cleanings.map((cleaning, idx) =>
-            <div key={cleaning.id} className="flex justify-between items-center p-3 hover:shadow transition-shadow">
-              <div className="basis-1/6">{idx + 1}.</div>
-              <div className="basis-2/6">{cleaning.from} - {cleaning.to}</div>
-              <div className="basis-3/6">Фамилия Имя</div>
-              {isEditing && <Button onClick={() => handleCleaningDelete(cleaning.id)}>X</Button>}
-            </div>
-          )}
-        </div>
+        <CleaningTable roomId={id} cleanings={room.data!.cleanings} />
         <h2 className="text-lg font-medium">QR-код</h2>
         <Image
           alt="qr"
@@ -83,12 +57,52 @@ export default function Room(
           }}
         />
       </div>
-      <AddCleaningModal roomId={id} isVisible={showAddCleaningModal}
-        onClose={() => {
-          setShowAddCleaningModal(false)
-          room.refetch()
-        }} />
+
     </main>
 
+  )
+}
+
+function CleaningTable({ roomId, cleanings }: { roomId: number, cleanings: Cleaning[] }) {
+  const [isEditing, setIsEditing] = useState(false)
+  const [showAddCleaningModal, setShowAddCleaningModal] = useState(false)
+
+  const utils = trpc.useContext();
+  const deleteCleaning = trpc.cleanings.deleteById.useMutation({
+    onSuccess: () => {
+      utils.rooms.byId.invalidate(roomId)
+    }
+  });
+
+  function handleCleaningDelete(id: number) {
+    deleteCleaning.mutate(id);
+  }
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-lg font-medium">График уборки</h2>
+        <div className="flex gap-2">
+          {isEditing
+            ? <Button onClick={() => setIsEditing(false)}>Отмена</Button>
+            : <Button onClick={() => setIsEditing(true)}>Изменить</Button>}
+          <Button onClick={() => setShowAddCleaningModal(true)}>Добавить</Button>
+        </div>
+      </div>
+      <div className="flex flex-col border rounded-md divide-y">
+        {cleanings.map((cleaning, idx) =>
+          <div key={cleaning.id} className="flex justify-between items-center p-3 hover:shadow transition-shadow">
+            <div className="basis-1/6">{idx + 1}.</div>
+            <div className="basis-2/6">{cleaning.from} - {cleaning.to}</div>
+            <div className="basis-3/6">Фамилия Имя</div>
+            {isEditing && <Button onClick={() => handleCleaningDelete(cleaning.id)}>X</Button>}
+          </div>
+        )}
+      </div>
+      <AddCleaningModal roomId={roomId} isVisible={showAddCleaningModal}
+        onClose={() => {
+          setShowAddCleaningModal(false)
+        }} />
+    </>
   )
 }
