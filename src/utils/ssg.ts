1997f540ed440ee3e07e317e842201129b9abb63
Vladislav Mironov <imaf6971@gmail.com>
2023-01-26 11:15:15 +0300

    added superjson transformer to ssg

--- src/utils/ssg.ts
+++ src/utils/ssg.ts
@@ -1,9 +1,11 @@
 import { createContext } from "@/server/context";
 import { appRouter } from "@/server/routers/_app";
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import superjson from "superjson";
 
-export const ssg = () => createProxySSGHelpers({
-  router: appRouter,
-  ctx: createContext(),
-  // transformer: superjson, // optional - adds superjson serialization
-});
+export const ssg = () =>
+  createProxySSGHelpers({
+    router: appRouter,
+    ctx: createContext(),
+    transformer: superjson,
+  });
