6e54ecba3e988955088729618753660b607dc97a
Vladislav Mironov <imaf6971@gmail.com>
2023-01-30 14:16:43 +0300

    using custom select

--- src/ui/Select.tsx
+++ src/ui/Select.tsx
@@ -1,39 +1,60 @@
-import { ChangeEventHandler, ReactElement } from "react";
+import { useState } from "react";
 
-type SelectProps = {
-  id: string;
-  name: string;
+type SelectOption = {
   label: string;
-  options: () => JSX.Element[] | undefined;
-  value: string | number | readonly string[];
-  onChange: ChangeEventHandler<HTMLSelectElement>;
+  value: string | number;
 };
 
-export default function Select({
-  id,
-  label,
-  options,
-  value,
-  onChange,
-  name,
-}: SelectProps) {
+type SelectProps = {
+  options: SelectOption[];
+  value?: SelectOption;
+  onChange: (value?: SelectOption) => void;
+};
+
+export default function Select({ options, value, onChange }: SelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function clearOptions() {
+    onChange(undefined);
+  }
+
+  function selectOption(option: SelectOption) {
+    onChange(option)
+  }
+
   return (
-    <div>
-      <label
-        htmlFor={id}
-        className="mb-2 block font-medium text-gray-900 dark:text-white"
+    <div
+      onBlur={() => setIsOpen(false)}
+      onClick={() => setIsOpen((prev) => !prev)}
+      className="relative flex min-h-[1.5em] w-80 items-center gap-2 rounded-md border p-2 transition-shadow hover:shadow focus:bg-gray-200 focus:outline-none focus:ring"
+    >
+      <span className="grow">{value?.label}</span>
+      <button
+        onClick={(e) => {
+          e.stopPropagation()
+          clearOptions();
+        }}
+        className="cursor-pointer p-0 text-xl hover:text-gray-200 focus:text-gray-200"
       >
-        {label}
-      </label>
-      <select
-        className="btn-like mb-6 block w-full bg-white"
-        value={value}
-        onChange={onChange}
-        name={name}
-        id={id}
+        &times;
+      </button>
+      <div className="w-[.05em] self-stretch bg-gray-200"></div>
+      <div className="border-[.25em] border-solid border-transparent border-t-gray-200"></div>
+      <ul
+        className={`absolute left-0 top-full m-0 mt-1 max-h-60 w-full overflow-y-auto rounded-md border border-gray-200 p-0 ${
+          isOpen ? "block" : "hidden"
+        }`}
       >
-        {options()}
-      </select>
+        {options.map((option) => (
+          <li onClick={(e) => {
+            e.stopPropagation()
+            selectOption(option)
+            setIsOpen(false)
+          }} key={option.value} className="cursor-pointer py-1 px-2">
+            {option.label}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
