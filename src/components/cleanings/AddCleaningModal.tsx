c3510c283061dde2e9f3de49d7338efb29dc754b
Vladislav Mironov <imaf6971@gmail.com>
2023-01-30 21:17:35 +0300

    going to custom select

--- src/components/cleanings/AddCleaningModal.tsx
+++ src/components/cleanings/AddCleaningModal.tsx
@@ -3,12 +3,18 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import Modal from "../../ui/Modal";
 import TimeInput from "@/ui/TimeInput";
 import SubmitInput from "@/ui/SubmitInput";
-import Select from "@/ui/Select";
+import Select, { SelectOption } from "@/ui/Select";
 
 type AddCleaningModalProps = {
   roomId: number;
   isVisible: boolean;
   onClose: () => void;
+};
+
+type Cleaner = {
+  id: number;
+  firstName: string;
+  lastName: string;
 };
 
 // TODO: add validation
@@ -26,12 +32,7 @@ export default function AddCleaningModal({
   const cleaners = trpc.staff.list.useQuery();
   const [from, setFrom] = useState(new Date(1970, 0, 1, 12, 0, 0, 0));
   const [to, setTo] = useState(new Date(1970, 0, 1, 12, 0, 0, 0));
-  const [cleanerId, setCleanerId] = useState(0);
-
-  function handleCleanerIdSelect(e: ChangeEvent<HTMLSelectElement>) {
-    const cleaner = parseInt(e.target.value);
-    setCleanerId(cleaner);
-  }
+  const [cleanerId, setCleanerId] = useState<number | undefined>(undefined);
 
   function handleToTimeInputChange(e: ChangeEvent<HTMLInputElement>) {
     setTo((prev) => {
@@ -55,8 +56,40 @@ export default function AddCleaningModal({
 
   function handleAddCleaningSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    addCleaning.mutate({ roomId, from, to, cleanerId });
+    if (cleanerId !== undefined) {
+      addCleaning.mutate({ roomId, from, to, cleanerId });
+    }
     onClose();
+  }
+
+  function selectedCleanerOption() {
+    const search = cleaners.data?.find((cleaner) => cleaner.id === cleanerId);
+    if (search === undefined) {
+      return undefined;
+    }
+    return cleanerToOption(search);
+  }
+
+  function cleanerToOption(cleaner: Cleaner): SelectOption {
+    return {
+      label: `${cleaner.firstName} ${cleaner.lastName}`,
+      value: cleaner.id,
+    };
+  }
+
+  function mapCleanersToSelectOptions() {
+    if (cleaners.isSuccess) {
+      return cleaners.data.map(cleanerToOption);
+    }
+    return [];
+  }
+
+  function handleCleanerChange(option?: SelectOption) {
+    if (option === undefined) {
+      setCleanerId(undefined);
+      return;
+    }
+    setCleanerId(option.value);
   }
 
   return (
@@ -81,18 +114,9 @@ export default function AddCleaningModal({
           />
         </div>
         <Select
-          label="Клинер"
-          name="staffId"
-          id="staffIdSelect"
-          value={cleanerId}
-          onChange={handleCleanerIdSelect}
-          options={() =>
-            cleaners.data?.map((cleaner) => (
-              <option value={cleaner.id} key={cleaner.id}>
-                {`${cleaner.firstName} ${cleaner.lastName}`}
-              </option>
-            ))
-          }
+          selectedOption={selectedCleanerOption()}
+          options={mapCleanersToSelectOptions()}
+          onChange={handleCleanerChange}
         />
         <SubmitInput value="Добавить" />
       </form>
