bda872d707b6d9b3aacd221c89d6023e5708472d
Vladislav Mironov <imaf6971@gmail.com>
2023-01-30 21:17:46 +0300

    formatting

--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -17,7 +17,10 @@ export default function Modal({ isVisible, onClose, children }: ModalProps) {
       onClick={onClose}
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur transition-all"
     >
-      <div onClick={(e) => e.stopPropagation()} className="w-4/5 md:w-3/5 xl:w-1/3">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="w-4/5 md:w-3/5 xl:w-1/3"
+      >
         <div className="rounded-md bg-white p-2 shadow">{children}</div>
       </div>
     </div>
