6e755ee35bb624641c79777a561bb648d60f5b59
Vladislav Mironov <imaf6971@gmail.com>
2023-01-31 11:34:16 +0300

    showing spinner on qr code load

--- src/components/room/RoomQR.tsx
+++ src/components/room/RoomQR.tsx
@@ -15,5 +15,17 @@ export default function RoomQR({ roomId }: RoomQRProps) {
     setLink(`${window.location.origin}/room/${roomId}/review/`);
   }, [roomId]);
 
-  return link !== "" ? <QRCodeSVG value={link} /> : <Spinner />;
+  return (
+    <div className=" flex justify-center">
+      {link !== "" ? (
+        <QRCodeSVG
+          className="h-1/2 w-1/2 md:h-1/3 md:w-1/3"
+          level="H"
+          value={link}
+        />
+      ) : (
+        <Spinner />
+      )}
+    </div>
+  );
 }
