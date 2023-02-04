ee76daa17bb8f937e60d14b75cb6520e3bf4eaea
Vladislav Mironov <imaf6971@gmail.com>
2023-02-04 12:04:30 +0300

    renamed subroutes in appRouter

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,12 +1,12 @@
 import { router } from "../trpc";
 import rooms from "./rooms";
-import cleanings from "./cleaning";
-import staff from "./staff";
+import cleaningPlan from "./cleaningPlan";
+import cleaners from "./cleaners";
 
 export const appRouter = router({
   rooms,
-  cleanings,
-  staff
+  cleaningPlan,
+  cleaners,
 });
 
 export type AppRouter = typeof appRouter;
